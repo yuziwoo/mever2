1c6f3ce3280b02125784a3b3efb6254995cf2782
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2022-02-25 15:04:34 +0900

    푸터 반응형 작업 예정

--- js/custom.js
+++ js/custom.js
@@ -51,7 +51,7 @@ window.addEventListener("DOMContentLoaded", (e) => {
               <li>
                 <a href="#">
                   <figure>
-                    <img src="img/apple.png" alt="플레이스토어 로고">
+                    <img src="img/apple.png" alt="애플 로고">
                   </figure>
                   <span>IOS</span>
                 </a>
@@ -129,5 +129,123 @@ window.addEventListener("DOMContentLoaded", (e) => {
     case "sub07" : header_list[6].classList.add("active");
     break;
   }
+
+
+  let footer = document.createElement("footer");
+  footer.innerHTML = `<div class="top">
+    <div class="gnb">
+      <ul>
+        <li>
+          <a href="index.html">
+            <span>홈페이지</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="sub04_invest.html">
+            <span>투자안내</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="sub05_cooperate.html">
+            <span>협력개발</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="sub-7_cs.html">
+            <span>고객센터</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="processing_policy.html">
+            <span>개인정보처리방침</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="sns">
+      <ul>
+        <li>
+          <a href="#">
+            <figure>
+              <img src="img/sns/youtube.png" alt="https://www.flaticon.com/kr/ 아이콘 제작자: Freepik - Flaticon">
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <figure>
+              <img src="img/sns/tiktok.png" alt="https://www.flaticon.com/kr/ 아이콘 제작자: Freepik - Flaticon">
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <figure>
+              <img src="img/sns/instagram.png" alt="https://www.flaticon.com/kr/ 아이콘 제작자: Freepik - Flaticon">
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <figure>
+              <img src="img/sns/facebook.png" alt="https://www.flaticon.com/kr/ 아이콘 제작자: Freepik - Flaticon">
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <figure>
+              <img src="img/sns/blog.png" alt="네이버 블로그">
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <figure>
+              <img src="img/sns/kakaotalk.png" alt="카카오톡">
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <figure>
+              <img src="img/sns/store.png" alt="네이버 스마트스토어">
+            </figure>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="line">
+
+  </div>
+  <div class="bottom">
+    <div class="left">
+      <div class="text">
+        <span>
+          meVer 대표 &nbsp;이상민<br>
+          고객센터 &nbsp;1688-8050 (평일 10시-18시)<br>
+          사업자등록번호 653-81-02062<br>
+        </span>
+      </div>
+      <div class="copyright">
+        <span>
+          Copyright © meVer All rights reserved.
+        </span>
+      </div>
+    </div>
+
+    <div class="right">
+      <figure>
+       <img src="img/logo_w3.png" alt="logo">
+      </figure>
+    </div>
+  </div>`;
+  document.body.appendChild(footer);
 
 })
